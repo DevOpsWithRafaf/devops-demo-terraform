69905262b777e3e990e9b69438dbfa4392a5ae21
Rafaf Tahsin <rafaftahsin@gmail.com>
2025-09-28 22:58:55 +0600

    added key-pair name

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "ostad-ec2-instance" {
   ami                    = var.ami-id
   instance_type          = "t3.large"
   vpc_security_group_ids = [var.sg-id]
+  key_name = var.key-name
   tags = {
     Name = "tf-demo-vm"
   }
