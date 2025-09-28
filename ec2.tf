7a6ce72b695f990b4a30dfd3afc320387a53bf11
Rafaf Tahsin <rafaftahsin@gmail.com>
2025-09-28 22:48:08 +0600

    updated code formation

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ostad-ec2-instance" {
-  ami = var.ami-id
-  instance_type = "t3.large"
-  vpc_security_group_ids = [ var.sg-id ]
+  ami                    = var.ami-id
+  instance_type          = "t3.large"
+  vpc_security_group_ids = [var.sg-id]
   tags = {
     Name = "tf-demo-vm"
   }
